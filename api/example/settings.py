cb76d84cefd013e4dfbde43795fa3d16d27b0c2a
Yosuf Ali <yosufali@users.noreply.github.com>
2025-03-10 22:24:23 +0000

    setup initial graphene.

--- api/example/settings.py
+++ api/example/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+GRAPHENE = {
+    "SCHEMA": "example.schema.schema"
+}
+
